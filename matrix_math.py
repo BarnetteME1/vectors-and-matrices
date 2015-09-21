9425cb5e8c56f67bf61802fe22150bd936006606
BarnetteME1 <BarnetteME1@gmail.com>
2015-09-21 15:12:55 -0400

    vectors_and_matrices.py

--- matrix_math.py
+++ matrix_math.py
@@ -2,8 +2,13 @@ class ShapeException(Exception):
     pass
 
 
-def dot():
-    pass
+def dot(vector_1, vector_2):
+    new_vector = []
+    if shape_exception_vxv(vector_1, vector_2) == True:
+        vec_len = range(0,len(vector_1))
+        for pos in vec_len:
+            new_vector.append(vector_1[pos]*vector_2[pos])
+    return reduce(lambda x, y: x+y, new_vector)
 
 
 def magnitude():
@@ -14,27 +19,42 @@ def shape():
     pass
 
 
-def vector_add():
-    pass
+def vector_add(vector_1, vector_2):
+    new_vector = []
+    if shape_exception_vxv(vector_1, vector_2) == True:
+        vec_len = range(0,len(vector_1))
+        for pos in vec_len:
+            new_vector.append(vector_1[pos] + vector_2[pos])
+    return new_vector
 
 
-def vector_sub():
-    pass
+def vector_sub(vector_1, vector_2):
+    new_vector = []
+    if shape_exception_vxv(vector_1, vector_2) == True:
+        vec_len = range(0,len(vector_1))
+        for pos in vec_len:
+            new_vector.append(vector_1[pos] - vector_2[pos])
+    return new_vector
 
 
 def vector_sum():
     pass
 
 
-def vector_multiply():
-    pass
+def vector_multiply(vector_1, scalar):
+    new_vector = []
+    if shape_exception_vxv(vector_1, vector_2) == True:
+        vec_len = range(0,len(vector_1))
+        for pos in vec_len:
+            new_vector.append(vector_1[pos] - vector_2[pos])
+    return new_vector
 
 
 def vector_mean():
     pass
 
 
-def matrix_row():
+def matrix_row(matrix):
     pass
 
 
@@ -42,8 +62,14 @@ def matrix_col():
     pass
 
 
-def matrix_scalar_multiply():
-    pass
+def matrix_vector_multiply(matrix, scalar):
+    new_matrix = []
+    for row in matrix:
+        new_row = 0
+        for column in row:
+            new_row += column * scalar
+        new_matrix.append(new_row)
+    return new_matrix
 
 
 def matrix_vector_multiply():
@@ -52,5 +78,3 @@ def matrix_vector_multiply():
 
 def matrix_matrix_multiply():
     pass
-
-
